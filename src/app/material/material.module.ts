2bb5b5e13a8ebece86952c24d2bcb9c410b0593a
Tenzin <gyurmey1835@gmail.com>
2021-02-17 00:00:39 +0100

    tables

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -7,6 +7,9 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatTableModule} from '@angular/material/table';
 
 
 const Material = [
@@ -17,8 +20,10 @@ const Material = [
   MatProgressSpinnerModule,
   MatToolbarModule,
   MatSidenavModule,
-  MatFormFieldModule
-  
+  MatFormFieldModule,
+  MatCardModule,
+  MatGridListModule,
+  MatTableModule
 ];
 
 @NgModule({
